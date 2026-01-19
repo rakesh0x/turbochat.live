5710735a08e8e8a95fdb605af0b94ce2f6b34656
Rakes <niu-24-14815@niu.edu.in>
2026-01-19 20:31:23 +0530

    vectorDB

--- interface/app/page.tsx
+++ interface/app/page.tsx
@@ -1,5 +1,7 @@
 export default function home() {
   return (
-    <div>hi there</div>
+    <div>
+      hi there
+    </div>
   )
 }
--- main.py
+++ main.py
@@ -1,74 +1,45 @@
 import requests
+import os
+from dotenv import load_dotenv, dotenv_values
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from scraper.fetch_html import get_data
+from pinecone import Pinecone
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
+pc = Pinecone(api_key="pcsk_27M6Rz_CvMfnWXnu3dKXoNHTHksNbXqXHkDAuHYVa67C7zu5YVdVrSXDaeBeDNWvJNKNf7")
 
-# -----------------------------
-# Fetch + save data
-# -----------------------------
 data = get_data("https://github.com/rakesh0x")
 
-with open("output.txt", "w", encoding="utf-8") as f:
-    f.write(data)
+index_name = "ragchatbot"
 
-# -----------------------------
-# Load documents
-# -----------------------------
-loader = TextLoader("output.txt")
-documents = loader.load()
+if not pc.has_index(index_name):
+    pc.create_index_for_model(:
+        name=index_name,
+        cloud="aws",
+        region="us-east-1",
+        embed={
+            "model": "text-embedding-3-small",
+            "field_map":{"text": "chunk_text"}
+        }
+    )
 
-# -----------------------------
-# Split documents
-# -----------------------------
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=500,
-    chunk_overlap=100
-)
-chunks = splitter.split_documents(documents)
-
-if not chunks:
-    print("I don't know (no content to index)")
-    exit()
-
-# -----------------------------
-# Vector store
-# -----------------------------
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-vectorstore = FAISS.from_documents(chunks, embeddings)
-
-# -----------------------------
-# Query
-# -----------------------------
 query = "what is git"
 
-docs_score = vectorstore.similarity_search_with_score(query, k=4)
-
-SIMILARITY_THRESHOLD = 0.3
-filtered_docs = []
-
-for doc, distance in docs_score:
-    similarity = 1 / (1 + float(distance))
-    if similarity >= SIMILARITY_THRESHOLD:
-        filtered_docs.append((doc, similarity))
-
-if not filtered_docs:
-    print("I don't know")
-    exit()
-
-context = "\n\n".join(
-    f"[score: {score:.2f}]\n{doc.page_content}"
-    for doc, score in filtered_docs
+results = index.search(
+    namespace="ns1",
+    query={
+        "top_k": 5,
+        "inputs": {
+            'text': query
+        }
+    }
 )
 
-# -----------------------------
-# Ollama
-# -----------------------------
+print(result)
+
 prompt = f"""
 Answer the question using ONLY the context below.
 If the answer is not present, say "I don't know".
@@ -86,4 +57,4 @@ response = requests.post(
 )
 response.raise_for_status()
 
-print(response.json()["response"]
+print(response.json()["response"])
